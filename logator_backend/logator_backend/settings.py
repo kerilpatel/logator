7926812cbfdc67ae4e08be10e96b8f4b479e4143
kerilpatel <kerilpatel@outlook.com>
2024-09-02 00:32:46 +0530

    fix: allowed incoming api requests from frontend

--- logator_backend/logator_backend/settings.py
+++ logator_backend/logator_backend/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = 'django-insecure-vwr%3)#)%3_n16%q&$x9ym!i3m7hc18hg7x+c^5q^=vf4$-aup
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'authentication',
     'service_registration',
     'log_ingestion'
@@ -53,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'logator_backend.urls'
